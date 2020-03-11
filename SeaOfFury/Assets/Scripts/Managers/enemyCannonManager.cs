3311bf6a753cbb713c8e821879ad591e2439982e
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2020-03-11 11:50:45 +0000

    enemyCannonManager Comments

--- SeaOfFury/Assets/Scripts/Managers/enemyCannonManager.cs
+++ SeaOfFury/Assets/Scripts/Managers/enemyCannonManager.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 
 public class enemyCannonManager : MonoBehaviour
 {   
+    //Both the player and enemy objects are added to the script along with both game master scripts.
     public GameObject player;
     public gameMaster gm;
-
     public GameObject enemy;
     public enemyGameMaster enemyGameMaster;
-    // Start is called before the first frame update
-
+    
+    //Just before the object is instantiated, both the enemy and player with teir respective game master scripts
+    //are set as the variables declared above.
     void Awake(){
         enemy = GameObject.FindGameObjectWithTag("attacker");
         enemyGameMaster = enemy.GetComponent<enemyGameMaster>();
@@ -19,10 +20,14 @@ public class enemyCannonManager : MonoBehaviour
         gm = player.GetComponent<gameMaster>();
     }
 
+    //Once the object is instantiated it will then be destoryed in 5 seconds time.
+    //This is to save memory so the game doesn't crash from all the cannon balls that are fired.
     void Start(){
         Destroy(gameObject, 5.0f);
     }
 
+    //When the enemy's cannon ball hits the player the health variable in the player's game master script has the damage
+    //stored in the game master script taken away from it.
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player"){
