4e528c31e382e048461f5e7214ca206b11824fdc
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2020-03-11 14:48:43 +0000

    playerManager Comments

--- SeaOfFury/Assets/Scripts/Managers/playerManager.cs
+++ SeaOfFury/Assets/Scripts/Managers/playerManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class playerManager : MonoBehaviour
 {
+    //Singleton is created to hold an instance of the player object at all times.
+    //This is used when the enemy AI are looking for the ships and 
+    //checking whether it's in the radius of the enemy ships.
     #region Singleton
 
     public static playerManager instance;
-
+    
     void Awake(){
         instance = this;
     }
