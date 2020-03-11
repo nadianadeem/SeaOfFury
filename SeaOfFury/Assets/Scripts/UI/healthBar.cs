e1bcb370270bde4afd86ca9c4761b337a444104d
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2020-03-11 12:57:19 +0000

    healthBar comments

--- SeaOfFury/Assets/Scripts/UI/healthBar.cs
+++ SeaOfFury/Assets/Scripts/UI/healthBar.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 
 public class healthBar : MonoBehaviour
 {
+    //Stores the slider of the health bar.
     public Slider slider;
 
+    //Thisthod set max health changes the max value of the slider to the parameter.
+    //the value of the slider is also set to the parameter so the slider is full.
     public void SetMaxHealth(int health){
         slider.maxValue = health;
         slider.value = health;
     }
 
+    //This method changes the value of the slider to the value given in the parameter.
     public void SetHealth(int health){
         slider.value = health;
     }
-
 }
