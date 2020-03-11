98c502a0968366f4a720ef1a8616d3c17dd19591
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2020-03-11 11:54:25 +0000

    enemyGameMaster comments

--- SeaOfFury/Assets/Scripts/Managers/enemyGameMaster.cs
+++ SeaOfFury/Assets/Scripts/Managers/enemyGameMaster.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class enemyGameMaster : MonoBehaviour
 {
+    //A public variable which is defined as an attacker class is created but nothing is stored yet.
+    //This is so other scripts can access the variables created for the attacker class.
+    
     public attackerClass attacker;
     // Start is called before the first frame update
     void Start()
     {
+        //When a ship is created they all have their own respective attacker class.
         attacker = new attackerClass();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if the attacker has no health left it is destroyed.
         if (attacker.health == 0){
                 Destroy(gameObject);
             }
